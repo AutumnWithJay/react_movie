1be7c9285e481a90fd19d5fd813ff67e0e47a0f1
Jay Park <devpark.0x1c@gmail.com>
2021-08-22 20:36:17 +0900

    Add: Loader, View of All Routes

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,1 +1,48 @@
-export default () => 'Home';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Section from 'Components/Section';
+import Loader from 'Components/Loader';
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
+
+const HomePresenter = ({ nowPlaying, upComing, popular, error, loading }) =>
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="현재상영중">
+          {nowPlaying.map((movie) => (
+            <span>{movie.title}</span>
+          ))}
+        </Section>
+      )}
+      {upComing && upComing.length > 0 && (
+        <Section title="상영예정작">
+          {upComing.map((movie) => (
+            <span>{movie.title}</span>
+          ))}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="인기상영작">
+          {popular.map((movie) => (
+            <span>{movie.title}</span>
+          ))}
+        </Section>
+      )}
+    </Container>
+  );
+
+HomePresenter.propTypes = {
+  nowPlaying: PropTypes.array,
+  upComing: PropTypes.array,
+  popular: PropTypes.array,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+};
+
+export default HomePresenter;
