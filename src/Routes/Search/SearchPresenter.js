9839be9a7eaa577eca49115eeadbc8ed6b9b746c
Jay Park <devpark.0x1c@gmail.com>
2021-08-22 20:57:58 +0900

    Add: Search Results

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,9 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Loader from 'Components/Loader';
+import Section from 'Components/Section';
 
-const SearchPresenter = ({ movieResults, tvResults, error, loading, searchTerm, handleSubmit }) =>
-  null;
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Form = styled.form`
+  width: 100%;
+  margin-bottom: 50px;
+`;
+
+const Input = styled.input`
+  all: unset;
+  width: 100%;
+  font-size: 24px;
+`;
+
+const SearchPresenter = ({
+  movieResults,
+  tvResults,
+  error,
+  loading,
+  searchTerm,
+  handleSubmit,
+  updateTerm,
+}) => (
+  <Container>
+    <Form onSubmit={handleSubmit}>
+      <Input
+        placeholder="영화나 TV 프로그램명을 입력해주세요"
+        value={searchTerm}
+        onChange={updateTerm}
+      ></Input>
+    </Form>
+    {loading ? (
+      <Loader />
+    ) : (
+      <>
+        {movieResults && movieResults.length > 0 && (
+          <Section title="영화 검색결과">
+            {movieResults.map((movie) => (
+              <span key={movie.id}>{movie.title}</span>
+            ))}
+          </Section>
+        )}
+        {tvResults && tvResults.length > 0 && (
+          <Section title="영화 검색결과">
+            {tvResults.map((show) => (
+              <span key={show.id}>{show.name}</span>
+            ))}
+          </Section>
+        )}
+      </>
+    )}
+  </Container>
+);
 
 SearchPresenter.propTypes = {
   movieResults: PropTypes.array,
@@ -12,6 +66,7 @@ SearchPresenter.propTypes = {
   loading: PropTypes.bool.isRequired,
   searchTerm: PropTypes.string,
   handleSubmit: PropTypes.func.isRequired,
+  updateTerm: PropTypes.func.isRequired,
 };
 
 export default SearchPresenter;
